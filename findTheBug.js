cb6d5515cbc90bb7ffaffdb2b543ba4ac7d2c547
Jeison <jeysonm021@gmail.com>
2023-06-23 10:27:35 -0500

    1 bug

---
+++ findTheBug.js
@@ -0,0 +1,12 @@
+
+function reverseNumber(number) {
+    let reversedNumber = '';
+
+    for (let i = 0; i <= number.length; i++) {
+        reversedNumber += number.charAt(number.length - i);
+    }
+
+    return reversedNumber;
+}
+
+console.log(reverseNumber(12345)); // Expected output: '54321'
